cf932ab64ee431fc75567d3e7a9ebcdda98dfaa5
wtlin1228 <wtlin1228@gmail.com>
2022-05-19 00:04:35 +0800

    style: element picker

--- src/js/scriptlet/contentscript.ts
+++ src/js/scriptlet/contentscript.ts
@@ -14,33 +14,87 @@ let candidates: string[] = [];
 let picker: HTMLDivElement | null;
 let elementSpecificityInput: HTMLInputElement | null;
 
-const createPicker = () => {
-  picker = document.createElement('div');
+const createPickerElement = () => {
+  const picker = document.createElement('div');
 
+  picker.style.background = '#2c3453';
+  picker.style.padding = '6px';
   picker.style.position = 'fixed';
   picker.style.width = '300px';
   picker.style.height = '160px';
   picker.style.top = '20px';
   picker.style.right = '20px';
-  picker.style.background = 'white';
   picker.style.zIndex = '200000';
 
-  elementSpecificityInput = document.createElement('input');
-  elementSpecificityInput.type = 'range';
-  elementSpecificityInput.min = '0';
-  elementSpecificityInput.max = '30';
+  return picker;
+};
 
-  const displayNameInput = document.createElement('input');
+const createElementSpecificityInput = () => {
+  const input = document.createElement('input');
 
-  const pickButton = document.createElement('button');
-  pickButton.innerText = 'Pick';
-  pickButton.onclick = () => {
+  input.type = 'range';
+  input.min = '0';
+  input.max = '30';
+
+  input.style.width = '100%';
+  input.style.display = 'inline-block';
+  input.style.accentColor = '#5e9eff';
+
+  return input;
+};
+
+const createDisplayNameInput = () => {
+  const input = document.createElement('input');
+
+  input.type = 'text';
+  input.placeholder = 'Name your slide';
+
+  input.style.width = '100%';
+  input.style.display = 'inline-block';
+  input.style.backgroundColor = 'white';
+  input.style.color = 'black';
+  input.style.padding = '1px 8px';
+  input.style.cursor = 'text';
+  input.style.fontSize = '16px';
+  input.style.fontWeight = '700';
+  input.style.lineHeight = '24px';
+  input.style.borderRadius = '0';
+
+  return input;
+};
+
+const createElementPickerButton = () => {
+  const button = document.createElement('button');
+
+  button.style.padding = '6px 12px';
+  button.style.borderRadius = '8px';
+  button.style.fontSize = '16px';
+  button.style.fontWeight = '600';
+  button.style.color = '#183153';
+  button.style.background = 'white';
+  button.style.lineHeight = '24px';
+  button.style.marginInlineStart = '4px';
+  button.style.cursor = 'pointer';
+
+  return button;
+};
+
+const createPickButton = () => {
+  const button = createElementPickerButton();
+
+  button.innerText = 'Pick';
+  button.onclick = () => {
     inspecting = true;
   };
 
-  const createButton = document.createElement('button');
-  createButton.innerText = 'Create';
-  createButton.onclick = () => {
+  return button;
+};
+
+const createCreateButton = (displayNameInput: HTMLInputElement) => {
+  const button = createElementPickerButton();
+
+  button.innerText = 'Create';
+  button.onclick = () => {
     hideOverlay();
 
     if (elementSpecificityInput) {
@@ -60,27 +114,51 @@ const createPicker = () => {
     }
   };
 
-  const quitButton = document.createElement('button');
-  quitButton.innerText = 'Quit';
-  quitButton.onclick = () => {
+  return button;
+};
+
+const createQuitButton = () => {
+  const button = createElementPickerButton();
+
+  button.innerText = 'Quit';
+  button.onclick = () => {
     destroyPicker();
   };
 
-  const displayNameContainer = document.createElement('div');
-  displayNameContainer.appendChild(displayNameInput);
+  return button;
+};
+
+const createPicker = () => {
+  // control the specificity of picked element
+  elementSpecificityInput = createElementSpecificityInput();
+
+  // display name of slide
+  const displayNameInput = createDisplayNameInput();
+
+  // action buttons
+  const pickButton = createPickButton();
+  const createButton = createCreateButton(displayNameInput);
+  const quitButton = createQuitButton();
 
   const buttonsContainer = document.createElement('div');
+  buttonsContainer.style.marginBlockStart = '12px';
   buttonsContainer.appendChild(pickButton);
   buttonsContainer.appendChild(createButton);
   buttonsContainer.appendChild(quitButton);
 
+  // picker itself
+  picker = createPickerElement();
   picker.appendChild(elementSpecificityInput);
-  picker.appendChild(displayNameContainer);
+  picker.appendChild(displayNameInput);
   picker.appendChild(buttonsContainer);
+
   document.body.appendChild(picker);
 };
 
 const destroyPicker = () => {
+  inspecting = false;
+  hideOverlay();
+
   if (picker !== null) {
     picker.remove();
     picker = null;
